520394cacc49503d3d40fb95d256900d4a49a25f
ALI736-WDS <ali.sh.p736@gmail.com>
2024-02-11 19:00:59 +0330

    Source Code JS Update 2023 added...

--- script.js
+++ script.js
@@ -1,130 +1,208 @@
-const todoInput = document.querySelector(".todo-input");
-const todoButton = document.querySelector(".todo-button");
-const todoList = document.querySelector(".todo-list");
-const filterOption = document.querySelector(".filter-todo");
+const taskInput = document.getElementById("task-input");
+const dateInput = document.getElementById("date-input");
+const addButton = document.getElementById("add-button");
+const editButton = document.getElementById("edit-button");
+const alertMessage = document.getElementById("alert-message");
+const todosBody = document.querySelector("tbody");
+const deleteAllButton = document.getElementById("delete-all-button");
+const filterButton = document.querySelectorAll(".filter-todos");
 
-todoButton.addEventListener("click", addTodo);
-todoList.addEventListener("click", deleteCompleteTodo);
-filterOption.addEventListener("click", filterTodo);
-document.addEventListener("DOMContentLoaded", getTodos);
+let todos = JSON.parse(localStorage.getItem("todos")) || [];
 
-function addTodo(event) {
-    event.preventDefault()
+const saveToLocalStorage = () => {
+  localStorage.setItem("todos", JSON.stringify(todos));
+};
 
-    const todoDiv = document.createElement("div");
-    todoDiv.classList.add("todo");
+const generateId = () => {
+  //kholase tar hamun aval hamasho return kardim
+  return Math.round(
+    Math.random() * Math.random() * Math.pow(10, 15)
+  ).toString();
+  // const id = Math.round(Math.random() * Math.random() * Math.pow(10, 15)).toString();
+  // // console.log(id);
+  // return id;
+};
 
-    const newTodo = document.createElement("li");
-    newTodo.innerText = todoInput.value;
+const showAlert = (message, type) => {
+  alertMessage.innerHTML = ""; //poshte sare ham ke alert bede, zire ham nayan, ghabli none beshe va jadide biad
+  const alert = document.createElement("p");
+  alert.innerText = message;
+  alert.classList.add("alert");
+  alert.classList.add(`alert-${type}`);
+  alertMessage.append(alert);
 
-    saveLocalTodo(todoInput.value);
+  setTimeout(() => {
+    alert.style.display = "none";
+  }, 2000);
+};
 
-    newTodo.classList.add("todo-item");
-    todoDiv.appendChild(newTodo);
-    todoInput.value = "";
+const displayTodos = (data) => {
+  //baraye ghesmate filter all, pernding, completed vorudi(data) midim
+  //migim agar vorudi(data) dade shod data ro ejra kon, dar gheire in surat todos hamishegi ejra kon
+  // const todoList = data ? data : todos;
+  const todoList = data || todos; //ya code bala ya in ke agar data nabud todos ejra beshe
+  todosBody.innerHTML = "";
+  // console.log(todoList);
+  // if (todos.length === 0) //sharte true false mide , === 0 tabdil be false mikone , agar ! bezarim behtare va tabdil be false mishe
+  //code herfeei tar dar zir:
+  //agar todos.length === 0 bashe , false hast va ejra nemishe , pas ! mizarim ke true beshe va ejra beshe
+  /* be zabune sade tar mishe goft: agar todos.length vojud nadasht yani === 0 bude , pas false hast va if ejra nemishe
+       pas ! mizarim ke true beshe va ejra beshe */
+  if (!todoList.length) {
+    todosBody.innerHTML = "<tr><td colspan='4'> No Task Found! </td></tr>";
+    return;
+  }
+  // console.log(todos);
 
-    const completedButton = document.createElement("button");
-    completedButton.innerHTML = "<i class='fas fa-check'></i>";
-    completedButton.classList.add("complete-btn");
-    todoDiv.appendChild(completedButton);
+  todoList.forEach((todo) => {
+    //baraye inke bug pish nayad va vaghti ezafe kardim, 2 3 ta baraye harkodum neshun nade
+    //baraye inke daghat meghdare akhar ro neshun nade va hame maghadir ro neshun bede (+) mikonim
+    todosBody.innerHTML += ` 
+        <tr>
+            <td> ${todo.task} </td>
+            <!-- <td> ${todo.date} ? ${todo.date} : "no date" </td> -->
+            <td> ${
+              todo.date || "No Date"
+            } </td>  <!-- code sade tar, agar avali false bud, pas dovomi ejra mishe -->
+            <td> ${todo.completed ? "Completed" : "Pending"} </td>
+            <td>
+                <button onclick="editHandler('${todo.id}')"> Edit </button>
+                <button onclick="toggleTotoHandler('${todo.id}')"> 
+                ${
+                  todo.completed ? "Undo" : "Do"
+                } <!-- obj dar aval false hast, agar anjam nashode bud, pas if true hast va Do miad va bar aks -->
+                </button>
+                <button onclick="deleteOneTodoHandler('${
+                  todo.id
+                }')"> Delete </button>
+            </td>
+        </tr>
+        `;
+  });
+};
 
-    const trashButton = document.createElement("button");
-    trashButton.innerHTML = "<i class='fas fa-trash'></i>";
-    trashButton.classList.add("trash-btn");
-    todoDiv.appendChild(trashButton);
-
-    todoList.appendChild(todoDiv);
-}
-
-function deleteCompleteTodo(event) {
-    const item = event.target;
-    console.log(item.parentElement)
-    if (item.classList[0] === "trash-btn") {
-        const todo = item.parentElement;
-        removeLocalTodo(todo);
-        todo.remove();
-    }
-    if (item.classList[0] === "complete-btn") {
-        const todo = item.parentElement;
-        todo.classList.toggle("completed");
-    }
-}
-
-function filterTodo(event) {
-    const todos = todoList.childNodes;
-    todos.forEach(function (todo) {
-        switch (event.target.value) {
-            case "all":
-                todo.style.display = "flex";
-                break;
-            case "completed":
-                if (todo.classList.contains("completed")) {
-                    todo.style.display = "flex";
-                } else {
-                    todo.style.display = "none";
-                }
-                break;
-            case "uncompleted":
-                if (todo.classList.contains("completed")) {
-                    todo.style.display = "none";
-                } else {
-                    todo.style.display = "flex";
-                }
-                break;
-        }
-    })
-}
-
-function saveLocalTodo(todo) {
-    let todos;
-    if (localStorage.getItem("todos") === null) {
-        todos = [];
-    } else {
-        todos = JSON.parse(localStorage.getItem("todos"));
-    }
+const addHandler = () => {
+  const task = taskInput.value;
+  const date = dateInput.value;
+  const todo = {
+    id: generateId(),
+    // task: task,  //chun key va value yek name hastan pas faghat key ro minevisim va JS khodesh tashkhis mide
+    task,
+    date,
+    completed: false,
+  };
+  if (task) {
+    //agar task = "", khode be khode false mishe chun == 0 hast
     todos.push(todo);
-    localStorage.setItem("todos", JSON.stringify(todos))
-}
+    saveToLocalStorage();
+    displayTodos();
+    taskInput.value = "";
+    dateInput.value = "";
+    // console.log(todos);
+    showAlert("Todo added successfully", "success");
+  } else {
+    // alert("Warning");
+    showAlert("Please enter a todo!", "error");
+  }
+};
 
-function removeLocalTodo(todo) {
-    let todos;
-    if (localStorage.getItem("todos") === null) {
-        todos = [];
-    } else {
-        todos = JSON.parse(localStorage.getItem("todos"));
-    };
-    const todoIndex = todo.children[0].innerText
-    todos.splice(todos.indexOf(todoIndex), 1);
-    localStorage.setItem("todos", JSON.stringify(todos));
-}
+const deleteAllHandler = () => {
+  if (todos.length) {
+    //agar length vojud dasht yani 1 2 3,... bud
+    todos = [];
+    saveToLocalStorage();
+    displayTodos();
+    showAlert("All todos cleared successfully", "success");
+  } else {
+    showAlert("No todos to clear", "error");
+  }
+};
 
-function getTodos() {
-    let todos;
-    if (localStorage.getItem("todos") === null) {
-        todos = [];
-    } else {
-        todos = JSON.parse(localStorage.getItem("todos"));
-    };
-    todos.forEach(function (todo) {
-        const todoDiv = document.createElement("div");
-        todoDiv.classList.add("todo");
+const deleteOneTodoHandler = (id) => {
+  const newTodos = todos.filter((todo) => todo.id !== id); //todo haee ro bede ke id hashun ba id click shode barabar nabashe
+  todos = newTodos; //...hame be gheire in click shode
+  saveToLocalStorage();
+  displayTodos();
+  showAlert("Todo deleted successfully", "success");
+};
 
-        const newTodo = document.createElement("li");
-        newTodo.innerText = todo;
+const toggleTotoHandler = (id) => {
+  // const newTodos = todos.map(todo => {
+  //     if (todo.id === id) {
+  //         // return {
+  //         //     id: todo.id,
+  //         //     task: todo.task,
+  //         //     date: todo.date,
+  //         //     completed: !todo.completed
+  //         // };
+  //         return {  //(code behtar 1)
+  //             ...todo,
+  //             completed: !todo.completed
+  //         }
+  //     } else {
+  //         return todo;
+  //     }
+  // });
+  // todos = newTodos; //ke bad az taghirat newTodo, dar const todos asli ham taghirat sabt shavad
+  const todo = todos.find((todo) => todo.id === id); //fine() avalin chizi ke peyda kone ro neshun mide va id ha unic hastan
+  todo.completed = !todo.completed; //obj refrence type hast, pas inja ke taghir kone, dare obj asli ham taghir dadae mishe
+  saveToLocalStorage();
+  displayTodos();
+  showAlert("Todo status changed successfully", "success");
+};
 
-        newTodo.classList.add("todo-item");
-        todoDiv.appendChild(newTodo);
+const editHandler = (id) => {
+  const todo = todos.find((todo) => todo.id === id);
+  // console.log(id)
+  taskInput.value = todo.task;
+  dateInput.value = todo.date;
+  addButton.style.display = "none";
+  editButton.style.display = "inline-block";
+  editButton.dataset.id = id;
+};
 
-        const completedButton = document.createElement("button");
-        completedButton.innerHTML = "<i class='fas fa-check'></i>";
-        completedButton.classList.add("complete-btn");
-        todoDiv.appendChild(completedButton);
+const applyEditHandler = (event) => {
+  const id = event.target.dataset.id;
+  const todo = todos.find((todo) => todo.id === id);
+  todo.task = taskInput.value;
+  todo.date = dateInput.value;
+  dateInput.value = "";
+  taskInput.value = "";
+  addButton.style.display = "inline-block";
+  editButton.style.display = "none";
+  saveToLocalStorage();
+  displayTodos();
+  showAlert("Todo edited successfully", "success");
+};
 
-        const trashButton = document.createElement("button");
-        trashButton.innerHTML = "<i class='fas fa-trash'></i>";
-        trashButton.classList.add("trash-btn");
-        todoDiv.appendChild(trashButton);
+const filterHandler = (event) => {
+  // console.log(event);
+  let filterTodos = null; //mitunim meghdar nadim ama agar null bedim hajme kamtari migire
+  const filter = event.target.dataset.filter;
 
-        todoList.appendChild(todoDiv)
-    })
-}
+  switch (filter) {
+    case "pending":
+      filterTodos = todos.filter((todo) => todo.completed === false); //filter pending
+      break;
+    case "completed":
+      filterTodos = todos.filter((todo) => todo.completed === true); //filter completed
+      break;
+    default:
+      filterTodos = todos; //filter all
+      break;
+  }
+
+  displayTodos(filterTodos);
+  // console.log(filterTodos);
+};
+
+//dar dadane vorudi data be displayTodos, ba clg gereftan, event ejra mishod chun inja addEventListener hast va event migire
+//pas arow func midim ke moshkeli pish nayad mage khodemun inja event bedim va estefade konim (e) => displayTodos(e));
+window.addEventListener("load", () => displayTodos());
+addButton.addEventListener("click", addHandler);
+deleteAllButton.addEventListener("click", deleteAllHandler);
+editButton.addEventListener("click", applyEditHandler);
+filterButton.forEach((button) => {
+  //3ta button begir va ruye 3ta event ro anjam bede
+  button.addEventListener("click", filterHandler);
+});
